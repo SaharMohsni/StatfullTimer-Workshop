589b8c435a0e3ce296fba4af6900e2cd2cf459a5
SaharMohsni <sahar.mohsni1990@gmail.com>
2019-01-28 15:25:22 +0100

    first commit of workshop

---
+++ src/components/TimerContaine.js
@@ -0,0 +1,54 @@
+import React from "react"
+import TimerView from "./TimerView"
+class TimerContainer extends React.Component {
+   constructor(){
+       super()
+       this.state={
+           timems:0,
+           interval:null
+       }
+    }
+       timeOn = () => {
+        this.setState({
+            timems: this.state.timems + 1000
+        })
+    }
+    start = () => {
+        
+            const intervalId = setInterval(this.timeOn, 1000) 
+            this.setState({
+                interval:intervalId
+            })
+
+            }
+   pause = () =>{
+        clearInterval(this.state.interval)
+        this.setState({
+            interval:null
+        })
+   } 
+   playOrPause = () =>{
+       return this.state.interval ? this.pause() : this.start()
+   }    
+    
+     reset = () =>{
+        this.setState({
+            timems:0
+        })
+        this.pause()
+     }
+
+    render() {  
+    return (
+        <div className="container">
+            <TimerView time={this.state.timems} />
+            <div className="btns">
+                <input  className="btn" style={this.state.interval?{color:"red"} : {color:"green"}} onClick={this.playOrPause} type="button" value={ this.state.interval? "Pause" : "Play"} />
+                <input  className="btn" onClick={this.reset} type="button" value="Reset" />
+            </div>
+        </div>   
+        ) ;    
+    
+    }
+}
+    export default TimerContainer
---
+++ src/components/TimerView.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "../App.css";
+
+const TimerConverter = milliseconds => {
+  let valTime = Math.floor(milliseconds / 1000);
+  // eslint-disable-next-line
+  let days = Math.floor(valTime / 86400);
+  let reshour = valTime % 86400;
+  let hours = Math.floor(reshour / 3600);
+  let minutes = Math.floor((valTime % 3600) / 60);
+  let seconds = (valTime % 3600) % 60;
+  return {
+    hours,
+    minutes,
+    seconds
+  };
+};
+
+const TimerView=(props)=>
+ 
+      <div className="background">
+        <div className="timer">
+          <div className="hours forme">
+            <span className="numeric">
+           { String(TimerConverter(props.time).hours).padStart(2, "0")}
+            </span>
+            <span className="letter">Hour</span>
+          </div>
+          <span>:</span>
+          <div className="minute forme">
+            <span className="numeric">
+            {String(TimerConverter(props.time).minutes).padStart(2, "0")}
+            </span>
+            <span className="letter">Minute</span>
+          </div>
+          <span>:</span>
+          <div className="second forme">
+            <span className="numeric">
+            {String(TimerConverter(props.time).seconds).padStart(2, "0")}
+            </span>
+            <span className="letter">Second</span>
+          </div>
+        </div>
+        
+      </div>
+ 
+export default TimerView;
